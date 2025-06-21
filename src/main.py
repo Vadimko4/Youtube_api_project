1c1a62959978168f693917d252ef0ff6fad6f2d8
vadimko4 <vad.informat@gmail.com>
2025-06-21 17:32:17 +0400

    update create_database, save_data_to_database

--- src/main.py
+++ src/main.py
@@ -10,13 +10,13 @@ def main():
     api_key = os.getenv("YT_API_KEY")
     channel_ids = [
         'UC-OVMPlMA3-YCIeg4z5z23A',  # moscowpython
-        # 'UCwHL6WHUarjGfUM_586me8w',  # highload
+        'UCwHL6WHUarjGfUM_586me8w',  # highload
     ]
     params = config()
 
     data = get_youtube_data(api_key, channel_ids)
     create_database('youtube', params)
-    # save_data_to_database(data, 'youtube', params)
+    save_data_to_database(data, 'youtube', params)
 
 
 if __name__ == '__main__':
