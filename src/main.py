b99b59fddfa94ce49e2428be4d0d7fe0b7e29bfe
vadimko4 <vad.informat@gmail.com>
2025-06-21 15:43:52 +0400

    update get_youtube_data

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import os
 from utils import get_youtube_data, create_database, save_data_to_database
 from src.config import config
+from dotenv import load_dotenv
 
 
 def main():
-    api_key = os.getenv('YT_API_KEY')
+    # api_key = os.getenv('YT_API_KEY')
+    load_dotenv(dotenv_path="C:/Users/vadimko4/PycharmProjects/Youtube_api_project/.env")
+    api_key = os.getenv("YT_API_KEY")
     channel_ids = [
         'UC-OVMPlMA3-YCIeg4z5z23A',  # moscowpython
         # 'UCwHL6WHUarjGfUM_586me8w',  # highload
@@ -12,7 +15,7 @@ def main():
     params = config()
 
     data = get_youtube_data(api_key, channel_ids)
-    # create_database('youtube', params)
+    create_database('youtube', params)
     # save_data_to_database(data, 'youtube', params)
 
 
--- src/utils.py
+++ src/utils.py
@@ -8,10 +8,26 @@ def get_youtube_data(api_key: str, channel_ids: list[str]) -> list[dict[str, Any
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     data = []
+    videos_data = []
+    next_page_token = None
     for channel_id in channel_ids:
         channel_data = youtube.channels().list(part='snippet, statistics', id=channel_id).execute()
 
-        print(channel_data)
+        while True:
+            response = youtube.search().list(part='id, snippet', channelId=channel_id, type = 'video',
+                                             order='date', maxResults=50, pageToken=next_page_token).execute()
+            videos_data.extend(response['items'])
+
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+
+        data.append({
+            'channel': channel_data['items'][0],
+            'videos': videos_data
+        })
+
+    return data
 
 
 def create_database(database_name: str, params: dict) -> None:
